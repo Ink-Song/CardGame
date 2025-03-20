91fdbc92c9c4a8c1f348a027625f7b26fa4a856c
Ink-Song <sunruh@proton.me>
2025-03-20 08:38:45 +0100

    Build Test: Attempting to export with dependencies (didn't work).

--- src/main/java/io/github/ink_song/idatt2003/CardGame/App.java
+++ src/main/java/io/github/ink_song/idatt2003/CardGame/App.java
@@ -6,7 +6,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
